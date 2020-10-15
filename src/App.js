450cf388abe11ad2cd6f26512dd1e0dce203de82
florentdesmarets <florent.desmarets@gmail.com>
2020-10-15 12:22:11 +0200

    je vais push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Contact from'./components/Contact.js';
+import ContactList from './components/ContactList';
+
+
+const contacts = [
+  {name: 'Peter alter', avatar: 'https://randomuser.me/api/portraits/men/45.jpg', online: false, key: 1},
+  {name: 'Bessie Long', avatar: 'https://randomuser.me/api/portraits/women/22.jpg', online: true, key: 2},
+  {name: 'Mae Alvarez', avatar: 'https://randomuser.me/api/portraits/women/64.jpg', online: false, key: 3},
+  {name: 'Ching John', avatar: 'https://randomuser.me/api/portraits/men/90.jpg', online: false, key: 4},
+  {name: 'Nicky Larson', avatar: 'https://randomuser.me/api/portraits/women/41.jpg', online: true, key: 5}
+]
 
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,20 @@ import  React from 'react';
 import './Contact.css';
 
 
-const name = "Zack Morrison";
-const avatar = "https://randomuser.me/api/portraits/men/51.jpg"
-const status = true; 
-
-function Contact() {
+function Contact(props) {
     return (
-        <figure className="Contact">
-            <img className="avatar" src={avatar}/>
-            <div className="status">
-                <h4 className="name">{name}</h4>
-                <span className="status-online"></span>
-                <p className="status-text">{status ? "Online" : "Offline"}</p>
-                
-            </div>
-        </figure>
-    )
-}
+      <figure className="Contact">
+        <img className="avatar" src={props.avatar} alt={props.character} />
+        <div className="status">
+          <h4 className="name">{props.name}</h4>
+          <span
+            className={props.online ? "status-online" : "status-offline"}
+          ></span>
+          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+        </div>
+      </figure>
+    );
+  }
+  
 
 export default Contact;
