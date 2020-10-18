f2fde45e6a89fe05abb0a94cf538e3eba9c829e2
florentdesmarets <florent.desmarets@gmail.com>
2020-10-18 17:03:50 +0200

    je push le challenge

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,37 @@
 import  React from 'react';
 import './Contact.css';
 
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: true,
 
-function Contact(props) {
+    };
+  }
+
+  render() {
     return (
       <figure className="Contact">
-        <img className="avatar" src={props.avatar} alt={props.character} />
+        <img className="avatar" src={this.props.avatar} alt={this.props.character} />
         <div className="status">
-          <h4 className="name">{props.name}</h4>
+          <h4 className="name">{this.props.name}</h4>
           <span
-            className={props.online ? "status-online" : "status-offline"}
+            className={this.state.online ? "status-online" : "status-offline"}
+            onClick={(event) => {
+              const newOnline = !this.state.online;
+              this.setState({ online: newOnline });
+            }}
           ></span>
-          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+          <p className="status-text"
+              onClick={(event) => {
+                const newOnline = !this.state.online;
+                this.setState({ online: newOnline });
+              }}>{this.state.online ? "Online" : "offline"}</p>
         </div>
       </figure>
     );
   }
-  
+}  
 
 export default Contact;
